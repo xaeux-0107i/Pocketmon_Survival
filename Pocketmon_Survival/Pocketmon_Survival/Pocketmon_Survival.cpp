dbc7f6c1a4fdda611bdd11739420ecb28b04d4a5
xaeux0107 <ekdms1324@gmail.com>
2024-05-24 17:59:23 +0900

    version.2

--- Pocketmon_Survival/Pocketmon_Survival/Pocketmon_Survival.cpp
+++ Pocketmon_Survival/Pocketmon_Survival/Pocketmon_Survival.cpp
@@ -1,10 +1,8 @@
-#pragma comment (lib, "msimg32.lib")
 #include <windows.h>
-#include "resource.h"
 
 HINSTANCE g_hInst;
 LPCTSTR lpszClass = L"Window Class Name";
-LPCTSTR lpszWindowName = L"Pocketmon Survival";
+LPCTSTR lpszWindowName = L"windows program 2";
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam);
 
@@ -42,106 +40,17 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdPa
 	return Message.wParam;
 }
 
-int C_direction = 0;
-int animationNum = 0;
-
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	PAINTSTRUCT ps;
-	HDC hDC, mDC, characterDC;
-	static HBITMAP hBitmapCharacter, hBitmapMDC;
-	RECT rt;
+	HDC hDC;
 
 	switch (uMsg) {
-	case WM_CREATE: {
-		SetTimer(hWnd, 1, 500, NULL);
-		hBitmapCharacter = LoadBitmap(g_hInst, MAKEINTRESOURCE(IDB_BITMAP1));
-	}
-				  break;
-	case WM_PAINT: {
-		GetClientRect(hWnd, &rt);
-		hDC = BeginPaint(hWnd, &ps);
-
-		// 주인공 포켓몬 이미지 가져오기
-		if (C_direction == 0) {
-			if (animationNum == 0) {
-				hBitmapCharacter = LoadBitmap(g_hInst, MAKEINTRESOURCE(IDB_BITMAP1));
-			}
-			else if (animationNum == 1) {
-				hBitmapCharacter = LoadBitmap(g_hInst, MAKEINTRESOURCE(IDB_BITMAP2));
-			}
-		}
-		else if (C_direction == 1) {
-			if (animationNum == 0) {
-				hBitmapCharacter = LoadBitmap(g_hInst, MAKEINTRESOURCE(IDB_BITMAP3));
-			}
-			else if (animationNum == 1) {
-				hBitmapCharacter = LoadBitmap(g_hInst, MAKEINTRESOURCE(IDB_BITMAP4));
-			}
-		}
-		else if (C_direction == 2) {
-			if (animationNum == 0) {
-				hBitmapCharacter = LoadBitmap(g_hInst, MAKEINTRESOURCE(IDB_BITMAP5));
-			}
-			else if (animationNum == 1) {
-				hBitmapCharacter = LoadBitmap(g_hInst, MAKEINTRESOURCE(IDB_BITMAP6));
-			}
-		}
-		else if (C_direction == 3) {
-			if (animationNum == 0) {
-				hBitmapCharacter = LoadBitmap(g_hInst, MAKEINTRESOURCE(IDB_BITMAP7));
-			}
-			else if (animationNum == 1) {
-				hBitmapCharacter = LoadBitmap(g_hInst, MAKEINTRESOURCE(IDB_BITMAP8));
-			}
-		}
-
-		characterDC = CreateCompatibleDC(hDC);
-		mDC = CreateCompatibleDC(hDC);
-		hBitmapMDC = CreateCompatibleBitmap(mDC, rt.right, rt.bottom);
-		SelectObject(characterDC, hBitmapCharacter);
-		SelectObject(mDC, hBitmapMDC);
-		FillRect(mDC, &rt, (HBRUSH)GetStockObject(WHITE_BRUSH));
-
-		BitBlt(hDC, 0, 0, 800, 600, mDC, 0, 0, SRCCOPY);
-		TransparentBlt(hDC, 400, 300, 50, 50, characterDC, 0, 0, 20, 20, RGB(255, 255, 255));
-
-		DeleteDC(characterDC);
-		DeleteDC(mDC);
-		DeleteObject(hBitmapCharacter);
-		DeleteObject(hBitmapMDC);
-		EndPaint(hWnd, &ps);
-	}
-				 break;
-	case WM_KEYDOWN:
-		switch (wParam) {
-		case VK_UP:
-			C_direction = 1;
-			break;
-		case VK_DOWN:
-			C_direction = 0;
-			break;
-		case VK_LEFT:
-			C_direction = 2;
-			break;
-		case VK_RIGHT:
-			C_direction = 3;
-			break;
-		}
-		InvalidateRect(hWnd, NULL, false);
+	case WM_CREATE:
 		break;
-	case WM_TIMER:
-		switch (wParam) {
-		case 1:
-			if (animationNum == 0) {
-				animationNum = 1;
-			}
-			else {
-				animationNum = 0;
-			}
-			InvalidateRect(hWnd, NULL, false);
-			break;
-		}
+	case WM_PAINT:
+		hDC = BeginPaint(hWnd, &ps);
+		EndPaint(hWnd, &ps);
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
