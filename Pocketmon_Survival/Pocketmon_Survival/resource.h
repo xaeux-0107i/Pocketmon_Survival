79b15d8b31f5bc885934977ded618e1628f82525
east_star00 <1219kdk@gmail.com>
2024-05-30 23:15:46 +0900

    version.9
    .

--- Pocketmon_Survival/Pocketmon_Survival/resource.h
+++ Pocketmon_Survival/Pocketmon_Survival/resource.h
@@ -27,12 +27,13 @@
 #define IDB_BITMAP23                    123
 #define IDB_BITMAP24                    124
 #define IDB_BITMAP25                    125
+#define IDB_BITMAP26                    126
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        126
+#define _APS_NEXT_RESOURCE_VALUE        127
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
